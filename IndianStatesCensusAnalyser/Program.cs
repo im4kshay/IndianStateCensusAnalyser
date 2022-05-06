14d5dd9f7d0012a7dd1c563c456f30b1317e0dc7
im4kshay <akshaysayre7@gmail.com>
2022-05-06 23:38:52 +0530

    Add project files.

---
+++ IndianStatesCensusAnalyser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=-=-=-=-=-=-=-=Welcome To The Indian State Census Analyser Problem=-=-=-=-=-=-=-=");
+        }
+    }
+}
